821c2dd2825ed3196514bb71f463789072dc40ee
subamathiR <subamathi881@gmail.com>
2026-07-09 09:47:24 +0530

    Create 3558-FindASafeWalkThroughAGrid

---
+++ 3558-FindASafeWalkThroughAGrid/3558-FindASafeWalkThroughAGrid.java
@@ -0,0 +1,43 @@
+// Last updated: 7/9/2026, 9:46:59 AM
+import java.util.*;
+class Solution {
+    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+        int m = grid.size();
+        int n = grid.get(0).size();
+        int[][] best = new int[m][n];
+        for (int[] row : best)
+            Arrays.fill(row, -1);
+        Queue<int[]> q = new LinkedList<>();
+        int startHealth = health - grid.get(0).get(0);
+        if (startHealth <= 0)
+            return false;
+        q.offer(new int[]{0, 0, startHealth});
+        best[0][0] = startHealth;
+        int[][] dir = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0];
+            int c = cur[1];
+            int h = cur[2];
+            if (r == m - 1 && c == n - 1)
+                return true;
+            for (int[] d : dir) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+                if (nr < 0 || nc < 0 || nr >= m || nc >= n)
+                    continue;
+                int newHealth = h - grid.get(nr).get(nc);
+                if (newHealth > 0 && newHealth > best[nr][nc]) {
+                    best[nr][nc] = newHealth;
+                    q.offer(new int[]{nr, nc, newHealth});
+                }
+            }
+        }
+        return false;
+    }
+}
