2aa42e2064727e65d55f96105d4760d11fc699d4
subamathiR <subamathi881@gmail.com>
2026-07-09 09:48:34 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:48:09 AM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
