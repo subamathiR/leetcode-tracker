0d4a26ed4e8fcf797178633f48585fd46510fe8a
subamathiR <subamathi881@gmail.com>
2026-07-09 09:47:19 +0530

    Create 3601-FindTheKThCharacterInStringGameIi

---
+++ 3601-FindTheKThCharacterInStringGameIi/3601-FindTheKThCharacterInStringGameIi.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 9:46:54 AM
+class Solution {
+    public char kthCharacter(long k, int[] operations) {
+        int shift = 0;
+        List<Long> lengths = new ArrayList<>();
+        long len = 1;
+
+        for (int op : operations) {
+            len *= 2;
+            lengths.add(len);
+            if (len >= k) break;
+        }
+
+        for (int i = lengths.size() - 1; i >= 0; i--) {
+            long half = lengths.get(i) / 2;
+            int op = operations[i];
+
+            if (k > half) {
+                k -= half;
+                if (op == 1) shift++;
+            }
+            
+        }
+
+        
+        return (char) ((('a' - 'a' + shift) % 26) + 'a');
+    }
+}
