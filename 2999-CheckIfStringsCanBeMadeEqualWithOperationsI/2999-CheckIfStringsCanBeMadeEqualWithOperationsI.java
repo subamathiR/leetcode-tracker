2248e7fcba87a9564e17fbff59a03a8dbbf8f753
subamathiR <subamathi881@gmail.com>
2026-07-09 09:48:18 +0530

    Create 2999-CheckIfStringsCanBeMadeEqualWithOperationsI

---
+++ 2999-CheckIfStringsCanBeMadeEqualWithOperationsI/2999-CheckIfStringsCanBeMadeEqualWithOperationsI.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:47:53 AM
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        int[] even = new int[26];
+        int[] odd = new int[26];
+        
+        for(int i = 0; i < s1.length(); i++){
+            if(i % 2 == 0){
+                even[s1.charAt(i) - 'a']++;
+                even[s2.charAt(i) - 'a']--;
+            } else {
+                odd[s1.charAt(i) - 'a']++;
+                odd[s2.charAt(i) - 'a']--;
+            }
+        }
+        
+        for(int i = 0; i < 26; i++){
+            if(even[i] != 0 || odd[i] != 0) return false;
+        }
+        
+        return true;
+    }
+}
