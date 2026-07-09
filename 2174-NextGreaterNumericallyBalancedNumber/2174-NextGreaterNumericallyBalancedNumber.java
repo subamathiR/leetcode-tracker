4901988867b246a9b0a723d7e4f0879fa76c225d
subamathiR <subamathi881@gmail.com>
2026-07-09 09:49:41 +0530

    Create 2174-NextGreaterNumericallyBalancedNumber

---
+++ 2174-NextGreaterNumericallyBalancedNumber/2174-NextGreaterNumericallyBalancedNumber.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 9:49:16 AM
+class Solution {
+    public int nextBeautifulNumber(int n) {
+        for(int i=n+1; i<=1224444; i++){
+            if(isBalance(i)){
+                return i;
+            }
+        }
+        return -1;
+    }
+private boolean isBalance(int x){
+
+    int[] count=new int[10];
+    while(x>0){
+   count[x%10]++;
+    x/=10;
+}
+
+for(int d=0; d<10; d++){
+    if(count[d]>0 && count[d] != d){
+        return false;
+    }
+}
+return true;
+}
+}
