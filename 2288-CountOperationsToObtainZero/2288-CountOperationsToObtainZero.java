dd6519ca807d77109b4b49ec0eec6ff674c75eba
subamathiR <subamathi881@gmail.com>
2026-07-09 09:49:22 +0530

    Create 2288-CountOperationsToObtainZero

---
+++ 2288-CountOperationsToObtainZero/2288-CountOperationsToObtainZero.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:48:57 AM
+ class Solution {
+    public int countOperations(int n1, int n2) {
+        int c = 0;
+        while (n1 > 0 && n2 > 0) {
+            c += n1 / n2;
+            n1 %= n2;
+            // swap num1 and num2
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
+        }
+        return c;
+    }
+}
