415fe3234e4d9986f5a39ec925861199e71634f7
subamathiR <subamathi881@gmail.com>
2026-07-09 09:45:36 +0530

    Create 4055-LongestBalancedSubstringI

---
+++ 4055-LongestBalancedSubstringI/4055-LongestBalancedSubstringI.java
@@ -0,0 +1,35 @@
+// Last updated: 7/9/2026, 9:45:11 AM
+class Solution {
+    public int longestBalanced(String s) {
+        int max = 0;
+        int n = s.length();
+        
+        for(int i = 0; i < n; i++){
+            int freq[] = new int[26];
+            
+            for(int j = i; j < n; j++){
+                char ch = s.charAt(j);
+                freq[ch - 'a']++;
+                
+                if(issamefreq(freq)){
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        return max;
+    }
+    
+    public static boolean issamefreq(int arr[]){
+        int value = 0;
+        
+        for(int a : arr){
+            if(a == 0) continue;
+            
+            if(value == 0) 
+                value = a;
+            else if(value != a) 
+                return false;
+        }
+        return true;
+    }
+}
