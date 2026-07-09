f778049b4b7db5bd361b9b9d0355df1f6a98adbe
subamathiR <subamathi881@gmail.com>
2026-07-09 09:48:13 +0530

    Create 3214-MaximizeAreaOfSquareHoleInGrid

---
+++ 3214-MaximizeAreaOfSquareHoleInGrid/3214-MaximizeAreaOfSquareHoleInGrid.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:47:48 AM
+ class Solution {
+    private int maxLen(int[] Bars) {
+        int count = 2, length = 2;
+        for(int i = 1; i < Bars.length; i++) {
+            if(Bars[i] - Bars[i-1] == 1) count++;
+            else count = 2;
+            length = Math.max(length, count);
+        }
+        return length;
+    }
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        Arrays.sort(hBars);
+        Arrays.sort(vBars);
+        int side = Math.min(maxLen(hBars), maxLen(vBars));
+        return side * side; 
+    }
+}
