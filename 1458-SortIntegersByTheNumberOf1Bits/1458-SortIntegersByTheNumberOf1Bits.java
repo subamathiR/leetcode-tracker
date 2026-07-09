c2c3f2f2d5cb0f62e7f9feccb03d5dff1b673642
subamathiR <subamathi881@gmail.com>
2026-07-09 09:51:35 +0530

    Create 1458-SortIntegersByTheNumberOf1Bits

---
+++ 1458-SortIntegersByTheNumberOf1Bits/1458-SortIntegersByTheNumberOf1Bits.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:51:10 AM
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        java.util.TreeMap<Integer, java.util.PriorityQueue<Integer>> bitmap = new java.util.TreeMap<>();
+        
+        for (int val : arr) {
+            int bits = Integer.bitCount(val);
+            bitmap.putIfAbsent(bits, new java.util.PriorityQueue<>());
+            bitmap.get(bits).add(val);
+        }
+        
+        int[] ans = new int[arr.length];
+        int index = 0;
+        
+        for (int key : bitmap.keySet()) {
+            java.util.PriorityQueue<Integer> pq = bitmap.get(key);
+            while (!pq.isEmpty()) {
+                ans[index++] = pq.poll();
+            }
+        }
+        return ans;
+    }
+}
