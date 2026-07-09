aab733f214189ace94f435229879ab12f768d893
subamathiR <subamathi881@gmail.com>
2026-07-09 09:47:12 +0530

    Create 3612-AdjacentIncreasingSubarraysDetectionI

---
+++ 3612-AdjacentIncreasingSubarraysDetectionI/3612-AdjacentIncreasingSubarraysDetectionI.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 9:46:47 AM
+public class Solution {
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int n = nums.size();
+        int inc = 1, prevInc = 0, maxLen = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums.get(i) > nums.get(i - 1)) inc++;
+            else {
+                prevInc = inc;
+                inc = 1;
+            }
+            maxLen = Math.max(maxLen, Math.max(inc >> 1, Math.min(prevInc, inc)));
+            if (maxLen >= k) return true;
+        }
+        return false;
+    }
+}
