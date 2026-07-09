05f1d8c0375340c73e4bfe6c453569237ac6a24d
subamathiR <subamathi881@gmail.com>
2026-07-09 09:49:34 +0530

    Create 2229-MaximumFruitsHarvestedAfterAtMostKSteps

---
+++ 2229-MaximumFruitsHarvestedAfterAtMostKSteps/2229-MaximumFruitsHarvestedAfterAtMostKSteps.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 9:49:09 AM
+class Solution {
+    public int maxTotalFruits(int[][] fruits, int startPos, int k) {
+        int left = 0, sum = 0, max = 0;
+
+        for (int right = 0; right < fruits.length; right++) {
+            sum += fruits[right][1];
+
+            while (left <= right && minSteps(fruits[left][0], fruits[right][0], startPos) > k) {
+                sum -= fruits[left][1];
+                left++;
+            }
+
+            max = Math.max(max, sum);
+        }
+
+        return max;
+    }
+
+    private int minSteps(int left, int right, int start) {
+        // Two paths: left first or right first
+        int goLeft = Math.abs(start - left) + (right - left);
+        int goRight = Math.abs(start - right) + (right - left);
+        return Math.min(goLeft, goRight);
+    }
+}
+
