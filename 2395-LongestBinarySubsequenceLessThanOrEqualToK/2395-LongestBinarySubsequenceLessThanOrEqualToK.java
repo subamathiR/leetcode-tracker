ebb2903bbaf04479e3819217021ae14a5da44080
subamathiR <subamathi881@gmail.com>
2026-07-09 09:49:06 +0530

    Create 2395-LongestBinarySubsequenceLessThanOrEqualToK

---
+++ 2395-LongestBinarySubsequenceLessThanOrEqualToK/2395-LongestBinarySubsequenceLessThanOrEqualToK.java
@@ -0,0 +1,27 @@
+// Last updated: 7/9/2026, 9:48:41 AM
+class Solution {
+    public int longestSubsequence(String s, int k) {
+        int n = s.length();
+        int zeros = 0;
+        int ones = 0;
+        long value = 0;
+        long pow = 1;
+        for (char c : s.toCharArray()) {
+            if (c == '0') zeros++;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == '1') {
+                if (value + pow > k) {
+                    continue; 
+                }
+                value += pow;
+                ones++;
+            }
+            pow <<= 1;
+            if (pow > k) {
+                break;
+            }
+        }
+        return zeros + ones;
+    }
+}
