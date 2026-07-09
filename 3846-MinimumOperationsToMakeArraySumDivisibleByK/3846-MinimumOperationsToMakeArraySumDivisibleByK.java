08ff6b656f15a6c9ebe5b6964dbc4140a162eeba
subamathiR <subamathi881@gmail.com>
2026-07-09 09:46:13 +0530

    Create 3846-MinimumOperationsToMakeArraySumDivisibleByK

---
+++ 3846-MinimumOperationsToMakeArraySumDivisibleByK/3846-MinimumOperationsToMakeArraySumDivisibleByK.java
@@ -0,0 +1,8 @@
+// Last updated: 7/9/2026, 9:45:48 AM
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int ans=0, n=nums.length;
+        for(int i=0; i<n; i++) ans+=nums[i];
+        return ans%k;
+    }
+}
