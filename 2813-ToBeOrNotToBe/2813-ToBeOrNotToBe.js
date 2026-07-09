64553c9a453d5cb31b767b33422cd406212e77fb
subamathiR <subamathi881@gmail.com>
2026-07-09 09:48:26 +0530

    Create 2813-ToBeOrNotToBe

---
+++ 2813-ToBeOrNotToBe/2813-ToBeOrNotToBe.js
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:48:01 AM
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+ var expect = function(val) {
+    return {
+        toBe: function(expected) {
+            if (val !== expected) throw new Error("Not Equal");
+            return true;
+        },
+        notToBe: function(expected) {
+            if (val === expected) throw new Error("Equal");
+            return true;
+        }
+    };
+};
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
