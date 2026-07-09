2e8dc3ed780111e9549b92b6fef2809d791ade2d
subamathiR <subamathi881@gmail.com>
2026-07-09 09:48:20 +0530

    Create 2887-SortVowelsInAString

---
+++ 2887-SortVowelsInAString/2887-SortVowelsInAString.java
@@ -0,0 +1,34 @@
+// Last updated: 7/9/2026, 9:47:55 AM
+class Solution {
+    public String sortVowels(String s) {
+        int[] freq = new int[128];
+        boolean[] isVowel = new boolean[128];
+
+        String vowels = "aeiouAEIOU";
+        for (char v : vowels.toCharArray()) {
+            isVowel[v] = true;
+        }
+
+        for (char c : s.toCharArray()) {
+            if (isVowel[c]) {
+                freq[c]++;
+            }
+        }
+
+        String order = "AEIOUaeiou";
+        int pos = 0;
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (isVowel[arr[i]]) {
+                while (pos < order.length() && freq[order.charAt(pos)] == 0) {
+                    pos++;
+                }
+                arr[i] = order.charAt(pos);
+                freq[order.charAt(pos)]--;
+            }
+        }
+
+        return new String(arr);
+    }
+}
