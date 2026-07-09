413e83bde6d208b09512b174fd9a9ef19c171a6d
subamathiR <subamathi881@gmail.com>
2026-07-09 09:50:09 +0530

    Create 2037-CountSquareSumTriples

---
+++ 2037-CountSquareSumTriples/2037-CountSquareSumTriples.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:49:44 AM
+class Solution {
+    public int countTriples(int n) {
+        int res = 0;
+        for (int u = 2; u * u <= n; u++) {
+            for (int v = 1; v < u; v++) {
+                if (((u - v) & 1) == 0 || gcd(u, v) != 1) continue;
+                
+                int c = u * u + v * v;
+                if (c > n) continue;
+                for (int k = 1; k * c <= n; k++)
+                    res += 2;
+            }
+        }
+        return res;
+    }
+
+    int gcd(int x, int y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+}
