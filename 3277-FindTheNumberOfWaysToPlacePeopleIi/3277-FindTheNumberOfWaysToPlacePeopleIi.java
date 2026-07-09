7b64c8797fd8c234dc934870d706cf4671618db7
subamathiR <subamathi881@gmail.com>
2026-07-09 09:48:01 +0530

    Create 3277-FindTheNumberOfWaysToPlacePeopleIi

---
+++ 3277-FindTheNumberOfWaysToPlacePeopleIi/3277-FindTheNumberOfWaysToPlacePeopleIi.java
@@ -0,0 +1,32 @@
+// Last updated: 7/9/2026, 9:47:36 AM
+import java.util.*;
+
+class Solution {
+    public int numberOfPairs(int[][] points) {
+        Arrays.sort(points, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(b[1], a[1]); 
+            }
+            return Integer.compare(a[0], b[0]); 
+        });
+
+        int pairCount = 0;
+        int n = points.length;
+
+       
+        for (int i = 0; i < n; i++) {
+            int upperY = points[i][1];   
+            int lowerYLimit = Integer.MIN_VALUE;
+            for (int j = i + 1; j < n; j++) {
+                int currentY = points[j][1];
+                if (currentY <= upperY && currentY > lowerYLimit) {
+                    pairCount++;
+                    lowerYLimit = currentY;
+                    if (currentY == upperY) break;
+                }
+            }
+        }
+
+        return pairCount;
+    }
+}
