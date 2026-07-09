5ad4737f7f8842c1fdb135655c0a8c0e804e3829
subamathiR <subamathi881@gmail.com>
2026-07-09 09:51:56 +0530

    Create 1302-DeleteCharactersToMakeFancyString

---
+++ 1302-DeleteCharactersToMakeFancyString/1302-DeleteCharactersToMakeFancyString.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:51:31 AM
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder ans = new StringBuilder();
+        char last = s.charAt(0);
+        ans.append(last);
+        int count = 1;
+        for (int i = 1; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == last) {
+                count++;
+                if (count < 3)
+                    ans.append(c);
+            } else {
+                last = c;
+                count = 1;
+                ans.append(c);
+            }
+        }
+        return ans.toString();
+    }
+} 
