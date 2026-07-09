d8631f048ccfd486ddd541a0e4ef63694d0551e7
subamathiR <subamathi881@gmail.com>
2026-07-09 09:47:26 +0530

    Create 3501-DeleteNodesFromLinkedListPresentInArray

---
+++ 3501-DeleteNodesFromLinkedListPresentInArray/3501-DeleteNodesFromLinkedListPresentInArray.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 9:47:01 AM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        boolean[] hasN=new boolean[100001];
+        for(int x: nums) hasN[x]=true;
+        ListNode dummy=new ListNode(0, head);
+        ListNode prev=dummy;
+        for(ListNode curr=head; curr!=null; curr=curr.next){
+            if (hasN[curr.val]) 
+                prev.next=curr.next;
+            else 
+                prev=prev.next;
+        }
+        return dummy.next;
+    }
+}
