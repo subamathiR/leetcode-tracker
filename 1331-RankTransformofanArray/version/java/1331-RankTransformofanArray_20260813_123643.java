bd02cefe49afa6089655732dbf110bcfadd466f8
subamathiR <subamathi881@gmail.com>
2026-08-13 12:36:44 +0530

    Create 1331-RankTransformofanArray

---
+++ 1331-RankTransformofanArray/version/java/1331-RankTransformofanArray_20260813_123643.java
@@ -0,0 +1,19 @@
+// Last updated: 8/13/2026, 12:36:43 PM
+1class Solution {
+2    public int[] arrayRankTransform(int[] arr) {
+3        int[] sorted = arr.clone();
+4        Arrays.sort(sorted);
+5        HashMap<Integer, Integer> map = new HashMap<>();
+6        int rank = 1;
+7        for (int i = 0; i < sorted.length; i++) {
+8            if (!map.containsKey(sorted[i])) {
+9                map.put(sorted[i], rank);
+10                rank++;
+11            }
+12        }
+13        for (int i = 0; i < arr.length; i++) {
+14            arr[i] = map.get(arr[i]);
+15        }
+16        return arr;
+17    }
+18}
