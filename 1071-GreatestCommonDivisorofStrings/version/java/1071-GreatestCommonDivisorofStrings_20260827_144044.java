764d0286071f875623a4f92b254192b16b4c630a
subamathiR <subamathi881@gmail.com>
2026-08-27 14:40:48 +0530

    Create 1071-GreatestCommonDivisorofStrings

---
+++ 1071-GreatestCommonDivisorofStrings/version/java/1071-GreatestCommonDivisorofStrings_20260827_144044.java
@@ -0,0 +1,11 @@
+// Last updated: 8/27/2026, 2:40:44 PM
+1class Solution {
+2    public int smallestEvenMultiple(int n) {
+3      if(n%2==0){
+4        return n;
+5      }
+6      else{
+7        return 2*n;
+8      }
+9    }
+10}
